6cc0227a043746a59d648726ab8c5481563a05de
Cheng Ya <chengya210@gmail.com>
2019-03-21 16:31:48 +0800

    重構function

--- product.py
+++ product.py
@@ -1,41 +1,52 @@
 #搜尋檔案
 import os # os = operating sysytem作業系統
 
-product = []
-if os.path.isfile('products.csv'):
-	print('有')
-	# 讀取檔案
+def read_file(filename):
+	product = []
+	if os.path.isfile(filename):
+		print('有')
+		# 讀取檔案
 
-	with open('products.csv', 'r', encoding = 'utf-8') as f: #編碼讀取跟寫入要相同
-		for line in f:
-			if '商品,價格' in line:
-				continue #跳到下一步
-			name, price = line.strip().split(',') #split(切割）','為切割標準 .strip(去掉換行)
-			product.append([name, price])
-	print(product)
-else:
-	print('無，現在開始產生新表單')
+		with open(filename, 'r', encoding = 'utf-8') as f: #編碼讀取跟寫入要相同
+			for line in f:
+				if '商品,價格' in line:
+					continue #跳到下一步
+				name, price = line.strip().split(',') #split(切割）','為切割標準 .strip(去掉換行)
+				product.append([name, price])
+		print(product)
+	else:
+		print('找不到檔案')
+	return product	
 
 
 #讓使用者輸入
-while True:
-	name = input('請輸入商品名稱：')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格:')	
-	p = [name, price]
-	#p = []
-	#p.append(name)
-	#p.append(price)
-	product.append(p)
-print(product)	
+def user_input(product):
+	while True:
+		name = input('請輸入商品名稱：')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格:')	
+		p = [name, price]
+		#p = []
+		#p.append(name)
+		#p.append(price)
+		product.append(p)
+	print(product)	
+	return product
 
 #印出所有購買紀錄
-for p in product:
-	print(p[0], '的價格是', p[1])
+def print_products(product):
+	for p in product:
+		print(p[0], '的價格是', p[1])
 
 #寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('商品,價格\n')
-	for p in product:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, product):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('商品,價格\n')
+		for p in product:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+product = read_file('products.csv')
+product = user_input(product)
+print_products(product)
+write_file('products.csv', product)
